0958bb6c789aec1666dbdf9ef1c6a412516361e9
monicajaimesc <monikmaja@gmail.com>
2019-08-29 04:09:09 +0000

    add read_input

---
+++ read_input.c
@@ -0,0 +1,21 @@
+#include "header.h"
+
+/**
+ * *read_line - read line for stdin
+ * Return: Success or Fail
+ */
+char *read_input(void)
+{
+	char *buffer = NULL;
+	int chars;
+	size_t sizebuffer = 0;
+
+	fflush(stdin);
+	chars = getline(&buffer, &sizebuffer, stdin);
+	if (chars == -1)
+	{
+		free(buffer);
+		return (NULL);
+	}
+	return (buffer);
+}
